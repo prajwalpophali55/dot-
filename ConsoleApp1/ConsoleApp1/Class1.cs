250e6d618bb1fd1d35b69d1e23999729329ba22c
unknown <prajwalpophali55@gmail.com>
2022-05-24 16:32:08 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {  
+       public static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 0, 1, 6, 5, 8, 15, 2 };
+            int largest = int.MinValue;
+            int second = int.MinValue;
+            foreach (int i in myArray)
+            {
+                if (i > largest)
+                {
+                    second = largest;
+                    largest = i;
+
+                }
+                else if (i > second)
+                    second = i;
+            }
+
+            {
+                System.Console.WriteLine(second);
+            }
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             string s, revs = "";
             Console.WriteLine("Enter string");
