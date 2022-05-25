7135621f9d44d2b97ad16e9c1154c81b7f6d6038
unknown <prajwalpophali55@gmail.com>
2022-05-25 14:44:25 +0530

    first commit

--- ConsoleApp1/ConsoleApp1/Class2.cs
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -6,9 +6,9 @@ namespace ConsoleApp1
 {
     class Class2
     {
-        public static void Main(string[] args)
+        public static void Main2(string[] args)
         {
-            int i, j, m, n;
+            int i, j;
             int[,] arr1 = new int[2, 2];
             int[,] arr2 = new int[2, 2];
             int[,] arr3 = new int[2, 2];
@@ -23,10 +23,10 @@ namespace ConsoleApp1
             Console.WriteLine();
             Console.WriteLine();
 
-            for (int row =0; row <2; row++);
+            for (int row =0; row <2; row++)
 
             {
-                for (int col =0; col <2; col++);
+                for (int col =0; col <2; col++)
                 {
                     Console.WriteLine("Please enter the value for arr2[" + row +"," + col +"]:");
                         arr2[row , col]= Convert.ToInt32(Console.ReadLine());
@@ -40,9 +40,9 @@ namespace ConsoleApp1
                 for(j =0; j <2; i++)
                 {
                     arr3[i, j] = 0;
-                    for(int k =0; k <2; k++)
+                    for( int k =0; k <2; k++)
                     {
-                        arr3[i, j] + = arr1[i, k] * arr2[k, j];
+                        arr3[i, j] +=arr1[i, k] * arr2[k, j];
                     }
                 }
             }
@@ -55,7 +55,7 @@ namespace ConsoleApp1
                 {
                     Console.Write(arr3[i, j] + "\t");
                 }
-                Console.Readline();
+                Console.WriteLine();
             }
         }
     }
---
+++ ConsoleApp1/ConsoleApp1/Class3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class3
+    {
+        public static void Main(string[] args)
+        {
+            int n, ctr = 0;
+            int[] arr1 = new int[100];
+            int i, j, k;
+            Console.Write("\n\nPrint all unique element of an array:\n");
+            Console.Write("\n\nPrint all unique element of an array:\n");
+            Console.Write("Input the number of elements to be stored in the array: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input {0} elements in the array: \n", n);
+            for(i = 0; i < n; i++)
+            {
+                Console.Write("element - {0}:", i);
+                arr1[i] = Convert.ToInt32(Console.ReadLine());
+
+                   
+            }
+            Console.Write("\nThe unique elements found in the array are: \n");
+            for(i = 0; i < n; i++)
+            {
+                ctr = 0;
+                {
+                    for(j=0; j<i-1; i++)
+                    {
+                        if(arr1[i] == arr1[j])
+                        {
+                            ctr++;
+                        }
+
+                    }
+                    for(k =i+1; k<n; k++)
+                    {
+                        if(arr1[i] == arr1[k])
+                        {
+                            ctr++;
+                        }
+                        {
+                            if(arr1[i] == arr1[i+1])
+                            {
+                                i++;
+                            }
+                        }
+                        if(ctr ==0)
+                        {
+                            Console.Write("{0}", arr1[i]);
+                        }
+                    }
+                }
+                Console.Write("n\n");
+            }
+
+        }
+    }
+}
