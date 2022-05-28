1e4a68e0ce40039dd872dbf8e01b2fed68af10a5
unknown <prajwalpophali55@gmail.com>
2022-05-28 16:15:31 +0530

    first commit

--- ConsoleApp 2/ConsoleApp 2/Class2.cs
+++ ConsoleApp 2/ConsoleApp 2/Class2.cs
@@ -14,29 +14,66 @@ namespace ConsoleApp_2
         public int ID;
         public string Gender;
         public int Salary;
+
+        public void GetData()
+        {
+            Console.WriteLine("Please Enter the Employe Details");
+            Console.WriteLine("please Enter the Employe Name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please Enter the Employe ID");
+            ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the Employe Gender");
+            Gender = Console.ReadLine();
+            Console.WriteLine("Please Enter the Employe Salary");
+            Salary = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Employe Details are");
+            Console.WriteLine("Name");
+            Console.WriteLine("Id");
+            Console.WriteLine("Gender");
+            Console.WriteLine("Salary");
+        }
     }
+        
     class Class2
     {
         static void Main(string[] args)
         {
-            sample s1 = new sample();
-            s1.a = 6;
+            /*  sample s1 = new sample();
+              s1.a = 6; 
 
-            Console.WriteLine(s1.a);
+              Console.WriteLine(s1.a);
 
-            //<class-name> <object-name>=new <class-Name>()
 
-            Employe E = new Employe();
-            E.Name = "Prajwal";
-            E.ID = 565654;
-            E.Gender = "male";
-            E.Salary = 20000;
 
-            Console.WriteLine("Employe Details are");
-            Console.WriteLine(E.Name);
-            Console.WriteLine(E.ID);
-            Console.WriteLine(E.Gender);
-            Console.WriteLine(E.Salary);
+              Employe E = new Employe();
+              E.Name = "Prajwal";
+              E.ID = 565654;
+              E.Gender = "male";
+              E.Salary = 20000;
+
+              Console.WriteLine("Employe Details are");
+              Console.WriteLine(E.Name);
+              Console.WriteLine(E.ID);
+              Console.WriteLine(E.Gender);
+              Console.WriteLine(E.Salary);*/
+            
+            
+            Employe[] Employelist = new Employe[2];
+            for (int i = 0; i < Employelist.Length; i++)
+            {
+                Employe E = new Employe();
+                E.GetData();
+                Employelist[i] = E;
+            }
+
+            for (int i = 0; i < Employelist.Length; i++)
+            {
+                Employelist[i].PrintData();
+            }
         }
     }
 }
