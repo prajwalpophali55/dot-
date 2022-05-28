c2d740a1baea11bc41455528e0aec3115a04ba95
unknown <prajwalpophali55@gmail.com>
2022-05-28 10:41:19 +0530

    first commit

---
+++ ConsoleApp 2/ConsoleApp 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 6;
+            Console.WriteLine("Before Swapping \na=" + x + "  b=" + y);
+            Swap(ref x, ref y);
+            Console.WriteLine("After Swapping \na=" + x + "  b=" + y);
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("In Swap Method \na=" + a + "  b=" + b);
+        }
+    }
+}
