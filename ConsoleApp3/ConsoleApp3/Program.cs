20b571f4aa1d0829caa17e297dda2fda846e4de6
unknown <prajwalpophali55@gmail.com>
2022-05-30 10:23:05 +0530

    first commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public static void sum()
+        {
+            int sum = 1 + 2;
+            Console.WriteLine(sum);
+        }
+        public static void sum(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+        public static void sum(int a, int b, int c)
+        {
+            int sum = a + b + c;
+            Console.WriteLine(sum);
+        }
+        public static void sum(string a, string b)
+        {
+            int sum = Convert.ToInt32(a) + Convert.ToInt32(b);
+            Console.WriteLine(sum);
+        }
+        public static void Main()
+        {
+            sum();
+            sum(3, 2, 3);
+            sum(3, 2);
+            sum("3", "2");
+        }
+    }
+}
