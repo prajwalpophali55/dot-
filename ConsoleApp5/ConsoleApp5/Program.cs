481f83c06a6bc218042f7b63b05cbbaa5706f257
unknown <prajwalpophali55@gmail.com>
2022-06-08 00:57:44 +0530

    first commit

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -9,16 +9,40 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             SampleDb1Context db = new SampleDb1Context();
-            // Sample1 tblsample = new Sample1();
-            //tblsample.Text = "Prajwal Pophali";
-            // db.Sample1s.Add(tblsample);
-            //  db.SaveChanges();
-            // Console.WriteLine();*/
+            /*Sample1 tblsample = new Sample1();
+            tblsample.Text = "Prajwal Pophali";
+             db.Sample1s.Add(tblsample);
+              db.SaveChanges();
+             Console.WriteLine();*/
 
 
             var data = db.Sample1s;
 
             Console.WriteLine("Values from database ");
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Id + " | " + item.Text);
+            }
+           /* Console.WriteLine("Pleae enter id of your name which you want to delete");
+            int DeleteItem = Convert.ToInt32(Console.ReadLine());
+            var DeleteObject = db.Sample1s.Where(x => x.Id == DeleteItem).FirstOrDefault();
+            db.Sample1s.Remove(DeleteObject);
+            db.SaveChanges();
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Id + " | " + item.Text);
+            }*/
+
+            Console.WriteLine("Pleae enter id of your name which you want to update");
+            int InsertItem = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the new name");
+            var newName = Console.ReadLine();
+            var UpdatteObject = db.Sample1s.Where(x => x.Id == InsertItem).FirstOrDefault();
+            UpdatteObject.Text = newName;
+            db.Sample1s.Update(UpdatteObject);
+            db.SaveChanges();
+
             foreach (var item in data)
             {
                 Console.WriteLine(item.Id + " | " + item.Text);
