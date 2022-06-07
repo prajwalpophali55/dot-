70f6bc6738d2bdc79d7343cbba62b64ef7e8f740
unknown <prajwalpophali55@gmail.com>
2022-06-08 00:29:57 +0530

    first commit

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 using System;
 using ConsoleApp5.Models;
-
+using System.Linq;
 namespace ConsoleApp5
 {
     class Program
@@ -9,11 +9,20 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             SampleDb1Context db = new SampleDb1Context();
-            Sample1 tblsample = new Sample1();
-            tblsample.Text = "Prajwal Pophali";
-            db.Sample1s.Add(tblsample);
-            db.SaveChanges();
-            Console.WriteLine();
+            // Sample1 tblsample = new Sample1();
+            //tblsample.Text = "Prajwal Pophali";
+            // db.Sample1s.Add(tblsample);
+            //  db.SaveChanges();
+            // Console.WriteLine();*/
+
+
+            var data = db.Sample1s;
+
+            Console.WriteLine("Values from database ");
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Id + " | " + item.Text);
+            }
         }
     }
 }
