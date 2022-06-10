7ab0b5af7d28fbb41f17b275647e4d5d9c07dd83
unknown <prajwalpophali55@gmail.com>
2022-06-10 16:56:35 +0530

    first commit

---
+++ first-sample-project/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Customer } from './app.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-sample-project';
+  myname='prajwal pophali'
+  CustomerModel:Customer=new Customer();
+}
+
