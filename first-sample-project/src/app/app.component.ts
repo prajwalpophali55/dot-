7c0865a9c4eea174de7f1021d561454754bb62db
unknown <prajwalpophali55@gmail.com>
2022-06-10 18:02:24 +0530

    first commit

--- first-sample-project/src/app/app.component.ts
+++ first-sample-project/src/app/app.component.ts
@@ -10,5 +10,22 @@ export class AppComponent {
   title = 'first-sample-project';
   myname='prajwal pophali'
   CustomerModel:Customer=new Customer();
+  CustmerModels: Array<Customer> = new Array<Customer>();
+  AddCustomer() {
+    this.CustmerModels.push(this.CustomerModel);
+    this.CustomerModel = new Customer();
+  }
+  EditCustomer(input: Customer) {
+    //console.log(input);
+    this.CustmerModels.shift();
+   this.CustomerModel=new Customer();
+
+  }
+  DeleteCustomer(input: Customer) {
+   // console.log(input);
+   this.CustmerModels.shift();
+   this.CustomerModel=new Customer();
+
+  }
 }
 
