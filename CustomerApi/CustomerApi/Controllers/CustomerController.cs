7b565546c29a82ae82e9adae3dcb4cfab461d8c5
unknown <prajwalpophali55@gmail.com>
2022-06-15 00:31:08 +0530

    first commit

---
+++ CustomerApi/CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using CustomerApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace CustomerApi.Controllers
+{
+   [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerDB1Context db;
+        public CustomerController(CustomerDB1Context _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return db.Customers;
+        }
+
+        [HttpPost]
+        public string Post([FromBody] Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return "success";
+        }
+
+    }
+}
